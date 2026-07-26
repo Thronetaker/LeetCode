b181a6dc2b112b0b4e89a070b5309c612f0ad23d
Radha Parmanand <parmanand224radha@gmail.com>
2026-07-26 21:02:38 +0530

    Time: 11 ms (96.03%) | Memory: 47.4 MB (91.92%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,23 +1,32 @@
 class Solution {
+    
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i = 0; i< nums.length ; i++){
             map.put( nums[i], map.getOrDefault(nums[i],0) + 1);
         }
 
-        PriorityQueue<int[]> minheap = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-
+        List<Integer> bucket[] = new List[nums.length+1];
+        //bucket[i] i is the number , & itsvalue is freq
         for( Map.Entry<Integer, Integer> set : map.entrySet()){
-            minheap.offer(new int[]{ set.getKey(), set.getValue() });
-            if(minheap.size() > k){
-                minheap.poll();
+            int freq = set.getValue();
+            if( bucket[freq] == null){
+                bucket[freq] = new ArrayList<>();
+            }
+            bucket[freq].add( set.getKey());
+        }
+
+        int idx = 0;
+        int res[] = new int[k];
+        for(int i = bucket.length - 1; i>=0 && idx < k ;i--){
+            if(bucket[i] != null){
+                for(int num : bucket[i]){
+                    res[idx++] = num;
+                    if( idx == k ) break;
+                }
             }
         }
-
-        int res[] = new int[k];
-        for( int i = k-1; i>=0;i--){
-            res[i] = minheap.poll()[0];
-        }
+   
         return res;
         
     }
