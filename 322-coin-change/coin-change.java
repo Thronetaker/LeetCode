15f816b3c8a762c3b2ffc870c2cc6091a764b507
Radha Parmanand <parmanand224radha@gmail.com>
2026-08-03 23:35:45 +0530

    Time: 18 ms (42.27%) | Memory: 46.2 MB (73.37%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        // Creating dp array of size amount+1
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        // Base case: dp[0] = 0
+        dp[0] = 0;
+
+        // Loop through all amounts from 1 to amount
+        for (int i = 1; i <= amount; i++) {
+            // Try each coin
+            for (int coin : coins) {
+                // If coin can be used
+                if (i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) {
+                    // Update dp[i] with minimum coins
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+                }
+            }
+        }
+
+        // If dp[amount] is still infinity, return -1
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
